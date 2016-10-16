183745bb6e505d22ed3a1ecba7e02cf1baea9bd9
Manuel Alonso Tarajano <tarajano@gmail.com>
2016-10-16 17:48:11 -0400

    .getMaxTempInMultipleFiles() implemented and working fine.

--- src/GetMaxTemperature.java
+++ src/GetMaxTemperature.java
@@ -1,6 +1,6 @@
 
 import edu.duke.*;
-
+import java.io.*;
 import org.apache.commons.csv.*;
 
 public class GetMaxTemperature {
@@ -8,7 +8,22 @@ public class GetMaxTemperature {
 	/**
 	 * @param args
 	 */
-
+	public static CSVRecord getMaxTempInMultipleFiles() {
+		CSVRecord max_temp_record = null;
+		CSVRecord record = null;
+		DirectoryResource dr = new DirectoryResource();
+		for (File file : dr.selectedFiles()){
+			FileResource input_file = new FileResource(file);
+			CSVParser file_parser = input_file.getCSVParser();
+			record = getMaxTempInFile(file_parser);
+			if(max_temp_record == null){
+				max_temp_record = record;
+			}else{
+				max_temp_record = getHighestOfTwoTemps(max_temp_record, record);
+			}
+		}
+		return max_temp_record;
+	}
 	public static CSVRecord getHighestOfTwoTemps(CSVRecord max_temp_record, CSVRecord record) {
 		double maxTemp = Double.parseDouble(max_temp_record.get("TemperatureF"));
 		double recordTemp = Double.parseDouble(record.get("TemperatureF"));
@@ -29,10 +44,15 @@ public class GetMaxTemperature {
 		return max_temp_record;
 	}
 	public static void main(String[] args) {
+		/*
 		FileResource input_file = new FileResource("nc_weather/2015/weather-2015-01-01.csv");
 		CSVParser file_parser = input_file.getCSVParser();
 		CSVRecord max_temp_record = getMaxTempInFile(file_parser);
 		System.out.println(max_temp_record.get("TimeEST")+" "+max_temp_record.get("TemperatureF"));
+		*/
+		
+		CSVRecord max_temp_record = getMaxTempInMultipleFiles();
+		System.out.println(max_temp_record.get("DateUTC")+" -- "+max_temp_record.get("TemperatureF"));
 	}
 
 }
